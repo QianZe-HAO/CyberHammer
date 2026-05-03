fd198b7750832760d03460c219357e1fe5e5e652
Zehao Qian <zehao.qian.cn@gmail.com>
2026-05-03 14:06:03 +0000

    format ui file

--- endpoint-ui.py
+++ endpoint-ui.py
@@ -2,17 +2,9 @@ import streamlit as st
 from pages import markdown_convert_page
 from pages import chat_agent_page
 
-
 pg = st.navigation(
     [
         st.Page(chat_agent_page, title="Deep Agent Chat"),
-
-
-
-
-
-
-
         st.Page(markdown_convert_page, title="Convert Files to Markdown"),
     ]
 )
